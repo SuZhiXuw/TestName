a16e2e2f2a224c082c6db8106913fd0f9aafd2c6
SuZhiXue <595734340@qq.com>
2016-11-14 21:40:10 +0800

    Initial Commit

---
+++ 使用git1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  使用git1
+//
+//  Created by 苏志学 on 2016/11/14.
+//  Copyright © 2016年 苏志学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 使用git1Tests/__git1Tests.m
@@ -0,0 +1,39 @@
+//
+//  __git1Tests.m
+//  使用git1Tests
+//
+//  Created by 苏志学 on 2016/11/14.
+//  Copyright © 2016年 苏志学. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __git1Tests : XCTestCase
+
+@end
+
+@implementation __git1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
